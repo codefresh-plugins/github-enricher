0cfc550fe6e6338dfd5096423ac53cfbe7abb265
pashavictorovich <pavel@codefresh.io>
2020-07-05 23:07:19 +0300

    change config

--- src/configuration/index.js
+++ src/configuration/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-    apiToken: process.env.CF_API_KEY || '5ee8c6ac8c93b8e09476606f.4ab93b0f27de416a67c7583b2e28677a',
+    apiToken: process.env.CF_API_KEY,
     host: process.env.CODEFRESH_HOST || 'https://g.codefresh.io',
-    image: process.env.IMAGE_SHA || 'sha256:5dc5a57ec85c31f5a26f762f4893826e7e6be13cd7c03bb8d469d052661dbd73',
+    image: process.env.IMAGE_SHA,
     branch: process.env.BRANCH,
     repo: process.env.REPO
 };
