78030edd26718b9a1396f243b64975a43f638845
nazar-codefresh <nazar@codefresh.io>
2020-08-31 19:12:07 +0300

    update cf url env name

--- src/configuration/index.js
+++ src/configuration/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     apiToken: process.env.CF_API_KEY,
-    host: process.env.CODEFRESH_HOST || 'https://g.codefresh.io',
+    host: process.env.CF_URL || 'https://g.codefresh.io',
     image: process.env.IMAGE_SHA,
     branch: process.env.BRANCH,
     repo: process.env.REPO,
