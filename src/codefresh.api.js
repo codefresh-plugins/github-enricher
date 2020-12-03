f5ace39d8c144964257157a5b6c8e14c77b45002
pashavictorovich <pavel@codefresh.io>
2020-12-03 17:34:54 +0200

    get committers from PR

--- src/codefresh.api.js
+++ src/codefresh.api.js
@@ -30,7 +30,7 @@ class CodefreshAPI {
                     value: {
                         url: pullRequest.url,
                         title: pullRequest.title,
-                        committer: pullRequest.committer,
+                        committers: pullRequest.committers,
                         commits: pullRequest.commits
                     }
                 },
