db57936bffbf9e300d7bef3a5a97a1edf1ceeef2
pashavictorovich <pavel@codefresh.io>
2021-04-05 12:40:38 +0300

    move to new way how to retrieve context

--- src/initializer.js
+++ src/initializer.js
@@ -4,16 +4,11 @@ const config = require('./configuration');
 class Initializer {
 
     async _prepareConfig() {
-        try {
-            const context = await codefreshApi.getContext(config.contextName);
-            const type = context.spec.type;
-            const token = context.spec.data.auth.password;
-            config.contextType = type;
-            config.contextCreds = token;
-        } catch (e) {
-
-        }
-
+        const context = await codefreshApi.getContext(config.contextName);
+        const type = context.spec.type;
+        const token = context.spec.data.auth.password;
+        config.contextType = type;
+        config.contextCreds = token;
     }
 
     async init() {
