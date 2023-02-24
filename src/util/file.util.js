75424076b532c89425a2a694c87ac9e6db70ca3a
andrii-codefresh <andrii@codefresh.io>
2023-02-24 17:25:47 +0200

    remove unused method

--- src/util/file.util.js
+++ src/util/file.util.js
@@ -3,12 +3,6 @@ const fs = require('fs');
 const configuration = require('../configuration')
 
 class FileUtil {
-
-    async getEventFile() {
-        const path = configuration.workingDirectory + '/event.json';
-        return this.fetchFile(path);
-    }
-
     async fetchFile(path) {
         try {
             return JSON.parse(await Promise.fromCallback((cb) => fs.readFile(path, 'utf8', cb)));
